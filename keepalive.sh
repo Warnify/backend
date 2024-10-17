b7b2c11f49c87a25dca7483d94de17ac1221e79f
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2024-10-17 01:20:27 -0400

    Update keep alive timer to 5 mins

--- keepalive.sh
+++ keepalive.sh
@@ -2,5 +2,5 @@
 
 while true;do
   curl https://backend-etxi.onrender.com/api/test
-  sleep 600  # Wait for 10 minutes before sending the next request
+  sleep 500  # Wait for < 10 minutes before sending the next request
 done
